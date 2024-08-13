2d240dedda23cd9a0769c3a888bc404c56aa2a49
Stas Vereskovsky <vereskovskystas@gmail.com>
2024-08-13 03:00:00 +0300

    Add work with gmail mailboxes instead of postfix

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM ruby:latest
 
-RUN gem install mail sinatra dotenv
-
 WORKDIR /app
+
+COPY Gemfile ./
+RUN bundle install
+
 COPY . .
+
+RUN mkdir -p logs
 
 EXPOSE 4567
 
-CMD ["rackup", "--host", "0.0.0.0"]
+CMD ["bundle", "exec", "rackup", "--host", "0.0.0.0", "-p", "4567"]
--- email_tracer.rb
+++ email_tracer.rb
@@ -2,52 +2,61 @@ require 'mail'
 require 'sinatra'
 require 'logger'
 require 'dotenv'
+require 'base64'
 
 Dotenv.load
 
 set :bind, '0.0.0.0'
 set :port, 4567
 
-before do
-  logger = Logger.new(STDOUT)
+Dir.mkdir("logs") unless Dir.exist?("logs")
+
+log_file = File.new("logs/access.log", "a+")
+log_file.sync = true
+logger = Logger.new(log_file)
+
+get '/img.png' do
+  logger.info "---------------------------------------------------------------"
+  logger.info "Image requested"
   logger.info "Request from IP: #{request.ip}"
   logger.info "User Agent: #{request.user_agent}"
   logger.info "Referer: #{request.referer}"
   logger.info "Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
   logger.info "Date and Time: #{Time.now}"
-end
+  logger.info "---------------------------------------------------------------"
 
-get '/img.png' do
   content_type 'image/png'
   send_file 'img.png'
 end
 
 options = {
-  address:              'postfix',
-  port:                 25,
-  domain:               'example.com',
-  authentication:       nil,
-  enable_starttls_auto: false
+  address:              'smtp.gmail.com',
+  port:                 587,
+  user_name:            ENV['GMAIL_USERNAME'],
+  password:             ENV['GMAIL_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true
 }
 
 Mail.defaults do
   delivery_method :smtp, options
 end
 
-sender_email = ENV['SENDER_EMAIL']
+sender_email = ENV['GMAIL_USERNAME']
 recipient_email = ENV['RECIPIENT_EMAIL']
 subject = 'Subject of the email'
-body = 'Body of the email'
-
-image_path = 'img.png'
-image_name = File.basename(image_path)
+body = "<html><body><p>Body of the email</p><img src=\"http://#{ENV['REMOTE_SERVER_IP']}:4567/img.png\"></body></html>"
 
 mail = Mail.new do
   from     sender_email
   to       recipient_email
   subject  subject
-  body     body
-  add_file filename: image_name, content: File.read(image_path)
+  html_part do
+    content_type 'text/html; charset=UTF-8'
+    body body
+  end
 end
 
 mail.deliver!
+
+logger.info "Email sent to #{recipient_email}"
